e927cb111b113bec7d7b1896f5ad92c14c2d4a7b
wen <ashley900121@gmail.com>
2025-07-18 00:00:53 +0800

    feat: update UDP target IP and port in boat side UDP sender

--- catkin_ws/src/rayvauav/src/udp_obspos_to_base.py
+++ catkin_ws/src/rayvauav/src/udp_obspos_to_base.py
@@ -4,8 +4,8 @@ import socket
 import struct
 from geometry_msgs.msg import PoseArray
 
-UDP_TARGET_IP = rospy.get_param("~target_ip", "140.113.148.80")
-UDP_TARGET_PORT = rospy.get_param("~udp_port", 49152)
+UDP_TARGET_IP = rospy.get_param("~target_ip", "192.168.0.113")
+UDP_TARGET_PORT = rospy.get_param("~udp_port", 49153)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -18,18 +18,18 @@ def posearray_callback(msg: PoseArray):
             x = pose.position.x
             y = pose.position.y
             z = pose.position.z
-            LENGTH = 13  # 1 (index) + 3*4 (float32 xyz)
 
-            packet = struct.pack('<BBBfffB', HEADER, LENGTH, idx, x, y, z, END)
+            packet = struct.pack('=BBBfffB', HEADER, 0, idx, x, y, z, END)
             sock.sendto(packet, (UDP_TARGET_IP, UDP_TARGET_PORT))
-            rospy.loginfo(f"[UDP] Sent Pose[{idx}] -> x: {x:.2f}, y: {y:.2f}, z: {z:.2f}")
+
+            rospy.loginfo(f"[UDP] Sent Pose[{idx}] -> x: {x:.6f}, y: {y:.6f}, z: {z:.6f} [len={len(packet)}]")
     except Exception as e:
         rospy.logerr(f"[UDP] Error sending PoseArray: {e}")
 
 def main():
     rospy.init_node('posearray_udp_sender', anonymous=True)
     rospy.Subscriber("/detected_obstacles/gps/pose_array", PoseArray, posearray_callback)
-    rospy.loginfo("Listening to /detected_obstacles/gps/pose_array and sending each Pose via UDP...")
+    rospy.loginfo(f"Listening to /detected_obstacles/gps/pose_array and sending each Pose via UDP to {UDP_TARGET_IP}:{UDP_TARGET_PORT}...")
     rospy.spin()
 
 if __name__ == "__main__":
