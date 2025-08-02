d471cf8275adda269195333e5e19a35871ec04da
wennnny <ashley900121@gmail.com>
2025-08-02 18:11:12 +0800

    feat: add goalpoint CoT generation and update listeners
    - Introduced goalpoint CoT generation utility in cot_utils.py.
    - Updated UDP listener to handle goalpoint packets.
    - Enhanced logging for received packets in udp_listener_async_49153.py.
    - Modified cot_config.json to include goalpoint configuration.

--- catkin_ws/src/rayvauav/src/udp_listener_async_49153.py
+++ catkin_ws/src/rayvauav/src/udp_listener_async_49153.py
@@ -3,7 +3,7 @@ import asyncio
 import socket
 import struct
 import logging
-from cot_utils import generate_obstacle_cot
+from cot_utils import generate_obstacle_cot, generate_goal_cot
 
 class ObstacleUDPWorker:
     def __init__(self, tx_queue, listen_ip="0.0.0.0", listen_port=49153):
@@ -17,7 +17,7 @@ class ObstacleUDPWorker:
             lambda: ObstacleProtocol(self.tx_queue),
             local_addr=(self.listen_ip, self.listen_port)
         )
-        logging.info(f"[UDP] Listening for obstacles on {self.listen_ip}:{self.listen_port}")
+        logging.info(f"[UDP] Listening for Pose packets on {self.listen_ip}:{self.listen_port}")
         while True:
             await asyncio.sleep(3600)
 
@@ -26,13 +26,27 @@ class ObstacleProtocol(asyncio.DatagramProtocol):
         self.tx_queue = tx_queue
 
     def datagram_received(self, data, addr):
-        if len(data) == 16 and data[0] == 0xAE:
-            try:
+        try:
+            if len(data) == 16:
+                header = data[0]
                 _, _, idx, x, y, z, _ = struct.unpack('=BBBfffB', data)
-                logging.info(f"[OBS] From {addr} → idx={idx}, x={x:.6f}, y={y:.6f}, z={z:.6f}")
-                cot = generate_obstacle_cot(idx, x, y, z)
+
+                if header == 0xAE:
+                    logging.info(f"[OBS] From {addr} → idx={idx}, x={x:.6f}, y={y:.6f}, z={z:.6f}")
+                    cot = generate_obstacle_cot(idx, x, y, z)
+
+                elif header == 0xAF:
+                    logging.info(f"[GOAL] From {addr} → idx={idx}, x={x:.6f}, y={y:.6f}, z={z:.6f}")
+                    cot = generate_goal_cot(idx, x, y, z)
+
+                else:
+                    logging.warning(f"[UNKNOWN HEADER] {header} from {addr}")
+                    return
+
                 asyncio.create_task(self.tx_queue.put(cot))
-            except Exception as e:
-                logging.error(f"[ERROR] Parsing obs packet: {e}")
-        else:
-            logging.warning(f"[IGNORED] {len(data)} bytes from {addr}: {data.hex()}")
+
+            else:
+                logging.warning(f"[IGNORED] Invalid size {len(data)} bytes from {addr}: {data.hex()}")
+
+        except Exception as e:
+            logging.error(f"[ERROR] Parsing packet from {addr}: {e}")
